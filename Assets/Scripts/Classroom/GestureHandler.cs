bbcb59a7e6c9a4a390b70c5a621a2d56566e5bfc
unknown <vitor.hbr@outlook.com>
2022-11-12 19:07:42 -0300

    fix: only master creates objects

--- Assets/Scripts/Classroom/GestureHandler.cs
+++ Assets/Scripts/Classroom/GestureHandler.cs
@@ -27,17 +27,20 @@ public class GestureHandler : MonoBehaviour
     private Vector3 _hiddenPosition = new Vector3(0, -10, 0);
     private GestureObject _leftObject = GestureObject.undefined;
     private GestureObject _rightObject = GestureObject.undefined;
-    private Vector3 penLeftCorrectPosition = new Vector3(-0.104f, -0.0296f, 0.0252f);
+    private Vector3 penLeftCorrectPosition = new Vector3(0.004f, 0.0296f, -0.0252f);
     private Vector3 penRightCorrectPosition = new Vector3(-0.104f, -0.0296f, -0.0252f);
     void Start()
     {
-        Pens = new List<GameObject>();
-        Erasers = new List<GameObject>();
-
-        for (int i = 0; i < 2; i++)
+        if (PhotonNetwork.IsMasterClient)
         {
-            Pens.Add(PhotonNetwork.Instantiate("Pen", new Vector3(0, 0, 0), Quaternion.identity));
-            Erasers.Add(PhotonNetwork.Instantiate("Eraser", new Vector3(0, 0, 0), Quaternion.identity));
+            Pens = new List<GameObject>();
+            Erasers = new List<GameObject>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Pens.Add(PhotonNetwork.Instantiate("Pen", new Vector3(0, 0, 0), Quaternion.identity));
+                Erasers.Add(PhotonNetwork.Instantiate("Eraser", new Vector3(0, 0, 0), Quaternion.identity));
+            }
         }
     }
 
@@ -45,7 +48,7 @@ public class GestureHandler : MonoBehaviour
     {
         if (_leftObject == GestureObject.pen)
         {
-            Pens[0].transform.position = _leftHand.position;
+            Pens[0].transform.position = _leftHand.position + penLeftCorrectPosition;
             Pens[0].transform.rotation = _leftHand.rotation;
             Pens[0].transform.Rotate(180f, 0f, 0f, Space.Self);
         }
@@ -61,7 +64,7 @@ public class GestureHandler : MonoBehaviour
 
         if (_rightObject == GestureObject.pen)
         {
-            Pens[1].transform.position = _rightHand.position;
+            Pens[1].transform.position = _rightHand.position + penRightCorrectPosition;
             Pens[1].transform.rotation = _rightHand.rotation;
         }
         else if (_rightObject == GestureObject.eraser)
